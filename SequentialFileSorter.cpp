a5546a65d00d1821d63b2a946cf9bbdc4e1dfdd7
Kepins <sz.kepinski1@gmail.com>
2022-11-26 18:13:01 +0100

    Completly changed merging

--- SequentialFileSorter.cpp
+++ SequentialFileSorter.cpp
@@ -47,44 +47,88 @@ void SequentialFileSorter::merge()
 	// Two records to read to
 	Record t1_record, t2_record;
 	// t1 must contain at least one record
-	t1->nextRecord(&t1_record);
+	int t1_empty = t1->nextRecord(&t1_record);
 	// t2 must contain at least one record
-	t2->nextRecord(&t2_record);
+	int t2_empty = t2->nextRecord(&t2_record);
 
-	// Bool value to store return value from tx->nextRecord
-	int empty;
-	while (true) {
+	// Store previous keys to determine if run has ended
+	int32_t prevkey_t1 = t1_record.key;
+	int32_t prevkey_t2 = t2_record.key;
+
+	while (!t1_empty && !t2_empty) {
+		// Check if record from t1 should be written to t3
 		if (t1_record.key < t2_record.key) {
 			// Write record from t1 to t3
 			t3->nextRecord(&t1_record);
+			// Store prev key from t1
+			prevkey_t1 = t1_record.key;
 			// Read next record from t1
-			empty = t1->nextRecord(&t1_record);
-			if (empty) {
-				// Write existing record from t2 to t3
+			t1_empty = t1->nextRecord(&t1_record);
+			// Check if run ended
+			if (!t1_empty && prevkey_t1 > t1_record.key) {
+				// Write whole run from t2 to t3
+
+				// Write current t2_record
 				t3->nextRecord(&t2_record);
-				// Write all other records from t2 to t3
-				while (!t2->nextRecord(&t2_record)) {
+				// Store prev key from t2
+				prevkey_t2 = t2_record.key;
+				// Read record from t2
+				t2_empty = t2->nextRecord(&t2_record);
+				while (!t2_empty && prevkey_t2 < t2_record.key) {
+					// Write next record from t2 to t3
 					t3->nextRecord(&t2_record);
+					// Store prev key from t2
+					prevkey_t2 = t2_record.key;
+					// Read record from t2
+					t2_empty = t2->nextRecord(&t2_record);
 				}
-				// Break from infinite loop
-				break;
 			}
 		}
+		// Record from t2 should be written to t3
 		else {
 			// Write record from t2 to t3
 			t3->nextRecord(&t2_record);
+			// Store prev key from t2
+			prevkey_t2 = t2_record.key;
 			// Read next record from t2
-			empty = t2->nextRecord(&t2_record);
-			if (empty) {
-				// Write existing record from t1 to t3
+			t2_empty = t2->nextRecord(&t2_record);
+			// Check if run ended
+			if (!t2_empty && prevkey_t2 > t2_record.key) {
+				// Write whole run from t1 to t3
+
+				// Write current t1_record
 				t3->nextRecord(&t1_record);
-				// Write all other records from t1 to t3
-				while (!t1->nextRecord(&t1_record)) {
+				// Store prev key from t1
+				prevkey_t1 = t1_record.key;
+				// Read record from t1
+				t1_empty = t1->nextRecord(&t1_record);
+				while (!t1_empty && prevkey_t1 < t1_record.key) {
+					// Write next record from t1 to t3
 					t3->nextRecord(&t1_record);
+					// Store prev key from t1
+					prevkey_t1 = t1_record.key;
+					// Read record from t1
+					t1_empty = t1->nextRecord(&t1_record);
 				}
-				// Break from infinite loop
-				break;
 			}
+		}
+	}
+	// Check if t1 has run out of records
+	if (t1_empty) {
+		//Write existing record from t2 to t3
+		t3->nextRecord(&t2_record);
+		// Write all other records from t2 to t3
+		while (!t2->nextRecord(&t2_record)) {
+			t3->nextRecord(&t2_record);
+		}
+	}
+	// t2 has run out of records
+	else {
+		// Write existing record from t1 to t3
+		t3->nextRecord(&t1_record);
+		// Write all other records from t1 to t3
+		while (!t1->nextRecord(&t1_record)) {
+			t3->nextRecord(&t1_record);
 		}
 	}
 	// t1, t2 will be used for distribiution 
